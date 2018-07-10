e560c1929fe1e4789e8ca4591e12b17a0baaa6cd
abelgoodwin1988 <abelgoodwin1988@gmail.com>
2018-07-09 22:38:13 -0600

    nav boilerplate, import views

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link> 
+      |
+      <router-link to="/concerts">Concerts</router-link>
+      |
       <router-link to="/about">About</router-link>
+      |
+      <router-link to="/auditions">Auditions</router-link>
     </div>
     <router-view/>
   </div>
--- src/router.js
+++ src/router.js
@@ -1,7 +1,10 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import Concerts from './views/Concerts.vue';
+import Home from './views/Home.vue';
 import About from './views/About.vue';
+import Auditions from './views/Auditions.vue';
 
 Vue.use(Router);
 
