1385514ae9f471615644115ca48bab018ea33c37
abelgoodwin1988 <abelgoodwin1988@gmail.com>
2018-07-10 22:48:17 -0600

    formatting and doubling up on her o

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,40 @@
 <template>
   <div id="app">
-    <nav class="navbar is-fixed-top" id="navbar" role="navigation" aria-label="main navigation">
-        <div class="navbar-brand has-text-primary">
-            <router-link class="navbar-item" to="/">Home</router-link>
-            <router-link class="navbar-item" to="/concerts">Concerts</router-link>
-            <router-link class="navbar-item" to="/about">About</router-link>
-            <router-link class="navbar-item" to="/auditions">Auditions</router-link>
+    <nav class="navbar is-fixed-top is-warning is-bold" id="navbar" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand has-text-primary is-large">
+            <a class="navbar-item">
+                <router-link class="navbar-item" to="/">Home</router-link>
+            </a>
+            <a class="navbar-item">
+                <router-link class="navbar-item" to="/concerts">Concerts</router-link>
+            </a>
+            <a class="navbar-item">
+                <router-link class="navbar-item" to="/about">About</router-link>
+            </a>
+            <a class="navbar-item">
+                <router-link class="navbar-item" to="/auditions">Auditions</router-link>
+            </a>
+            <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+                <span aria-hidden="true"><router-link class="navbar-item" to="/">Home</router-link></span>
+                <span aria-hidden="true"><router-link class="navbar-item" to="/concerts">Concerts</router-link></span>
+                <span aria-hidden="true"><router-link class="navbar-item" to="/auditions">Auditions</router-link></span>
+            </a>
         </div>
     </nav>
     <router-view/>
+    <footer class="footer">
+        <div class="content has-text-centered">
+            <p>
+                <strong>Colorado Mormon Chorale</strong> - Denver, CO.
+            </p>
+            <p>
+                Copyright © 2000-2018 <strong>Colorado Mormon Chorale</strong>
+            </p>
+            <p>
+                <strong>Privacy Policy</strong>
+            </p>
+        </div>
+    </footer>
   </div>
 </template>
 
