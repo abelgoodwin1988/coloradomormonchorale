ff0eaba2bd9be6db3d4fa87b456822f2bce88d15
abelgoodwin1988 <abelgoodwin1988@gmail.com>
2018-07-11 00:49:40 -0600

    fix router

--- src/router.js
+++ src/router.js
@@ -2,6 +2,8 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
 import About from './views/About.vue';
+import Concerts from './views/Concerts.vue';
+import Auditions from './views/Auditions.vue';
 
 Vue.use(Router);
 
@@ -13,9 +15,19 @@ export default new Router({
       component: Home,
     },
     {
+      path: '/concerts',
+      name: 'conerts',
+      component: Concerts,
+    },
+    {
       path: '/about',
       name: 'about',
       component: About,
+    },
+    {
+      path: '/auditions',
+      name: 'auditions',
+      component: Auditions,
     },
   ],
 });
