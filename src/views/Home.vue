d2d3db8d5b75916d162955936f2f5de6388221c7
abelgoodwin1988 <abelgoodwin1988@gmail.com>
2018-07-12 18:50:38 -0600

    reduce below 100 characters/line

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,7 +10,12 @@
                                         The Colorado Mormon Chorale
                                     </h1>
                                     <h2 class="subtitle">
-                                        The COLORADO MORMON CHORALE, now in its 36th year, consists of people from all walks of life, joined by a common bond of commitment to share their spirit through music. The Chorale sings year 'round, and has presented over 600 performances in Denver and Colorado, bringing a love of life and fellow man to many thousands.
+                                        The <strong>Colorado Mormon Chorale</strong>, now in its
+                                        36th year, consists of people from all walks of life,
+                                        joined by a common bond of commitment to share their spirit
+                                        through music. The Chorale sings year-round, and has
+                                        presented over 600 performances in Denver and Colorado,
+                                        bringing a love of life and fellow man to many thousands.
                                     </h2>
                                 </div>
                             </div>
