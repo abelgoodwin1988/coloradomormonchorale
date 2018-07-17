c30c900ae38657201377263ba2065f3b266a927d
Jacob Goodwin <jacob.goodwin@gmail.com>
2018-07-17 15:58:57 -0600

    Setup single event with faker data in vuex store

--- src/store.js
+++ src/store.js
@@ -1,11 +1,18 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import faker from 'faker';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    event: {
+      title: 'The event title',
+      date: faker.date.between('2018-08-01', '2018-12-01'),
+      imageURL: faker.image.imageUrl(),
+      additionalInfo: 'Additional info',
+      featured: faker.random.boolean(),
+    },
   },
   mutations: {
 
