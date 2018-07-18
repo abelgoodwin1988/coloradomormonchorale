69e2fc671ce1e2d8896c15c53c9fa41b72d6cc99
Jacob Goodwin <jacob.goodwin@gmail.com>
2018-07-17 22:51:46 -0600

    Add initState mutation to create data beforeCreate on App.vue

--- src/store.js
+++ src/store.js
@@ -1,23 +1,27 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import faker from 'faker';
+// import faker from 'faker';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    event: {
-      title: 'The event title',
-      date: faker.date.between('2018-08-01', '2018-12-01'),
-      imageURL: faker.image.imageUrl(),
-      additionalInfo: 'Additional info',
-      featured: faker.random.boolean(),
-    },
+    count: 0,
+    events: [], // an array of events that will eventually be retrieved from a database
+    // event: {
+    //   // break date up into date, day of week, time
+    //   title: faker.lorem.words(3),
+    //   date: faker.date.between('2018-08-01', '2018-12-01'), // returns a javascript Date
+    //   imageURL: faker.image.imageUrl(),
+    //   additionalInfo: faker.lorem.words(4),
+    //   featured: faker.random.boolean(),
+    // },
   },
   mutations: {
-
+    initState(state, payload) {
+      state.events = payload;
+    },
   },
   actions: {
-
   },
 });
