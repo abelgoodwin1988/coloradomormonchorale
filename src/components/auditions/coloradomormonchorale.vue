1975aeba290db21ba82aa40d7834e2f946d5caff
abelgoodwin1988 <abelgoodwin1988@gmail.com>
2018-07-18 20:20:05 -0600

    mock form for log

--- src/components/auditions/coloradomormonchorale.vue
+++ src/components/auditions/coloradomormonchorale.vue
@@ -1,5 +1,93 @@
 <template>
     <section class="section">
-        cmc
+        <div class="columns">
+            <div class="column is-one-quarter">
+                1/4
+            </div>
+            <div class="column">
+                <div class="field-body">
+                    <div class="field">
+                        <label class="label has-text-white">First Name</label>
+                        <div class="control">
+                            <input class="input" type="text" placeholder="First Name">
+                        </div>
+                    </div>
+                    <div class="field">
+                        <label class="label has-text-white">Last Name</label>
+                        <div class="control">
+                            <input class="input" type="text" placeholder="Last Name">
+                        </div>
+                    </div>
+                </div>
+                <div class="field">
+                    <label class="label has-text-white">Address</label>
+                    <div class="control">
+                        <input class="input" type="text" placeholder="Address">
+                    </div>
+                </div>
+                <div class="field">
+                    <label class="label has-text-white">City</label>
+                    <div class="control">
+                        <input class="input" type="text" placeholder="City">
+                    </div>
+                </div>
+                <div class="field">
+                    <label class="label has-text-white">State</label>
+                    <div class="control">
+                        <input class="input" type="text" placeholder="State">
+                    </div>
+                </div>
+                <div class="field">
+                    <label class="label has-text-white">ZIP</label>
+                    <div class="control">
+                        <input class="input" type="text" placeholder="Zip">
+                    </div>
+                </div>
+                <!-- PHONE  -->
+                <div class="field is-horizontal">
+                    <div class="field-label is-normal">
+                        <label class="label has-text-white">Phone</label>
+                    </div>
+                    <div class="field-body">
+                        <div class="field">
+                            <div class="control">
+                                <input class="input" type="text" placeholder="Phone">
+                            </div>
+                        </div>
+                        <div class="field is-narrow">
+                            <div class="control">
+                                <div class="select is-fullwidth">
+                                <select>
+                                    <option>Home</option>
+                                    <option>Cell</option>
+                                    <option>Work</option>
+                                </select>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="field">
+                    <label class="label has-text-white">Email</label>
+                    <div class="control">
+                        <input class="input" type="text" placeholder="Email">
+                    </div>
+                </div>
+                <p>Current Chorale Members:</p>
+                <div class="field">
+                    <label class="label has-text-white">Membership Statuis</label>
+                    <div class="control">
+                        <input class="input" type="text" placeholder="Membership Status">
+                    </div>
+                </div>
+                <div class="field">
+                    <label class="label has-text-white">Voice Section</label>
+                    <div class="control">
+                        <input class="input" type="text" placeholder="Voice Section">
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
 </template>
--- src/components/auditions/lambofgod.vue
+++ src/components/auditions/lambofgod.vue
@@ -1,5 +1,23 @@
 <template>
     <section class="section">
-        log
+        <div class="columns">
+            <div class="column is-one-quarter">
+                1/4
+            </div>
+            <div class="column">
+                <p>First Name:</p>
+                <p>Last Name:</p>
+                <p>Address:</p>
+                <p>City:</p>
+                <p>State:</p>
+                <p>ZIP:</p>
+                <p>Phone:</p>
+                <p>Phone Type:</p>
+                <p>E-Mail:</p>
+                <p>Chorale</p>
+                <p>Membership Status:</p>
+                <p>Vocal Section:</p>
+            </div>
+        </div>
     </section>
 </template>
