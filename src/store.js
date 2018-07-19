71df4cf22a85eda2cb01ebed2e00332cb9157ff5
Jacob Goodwin <jacob.goodwin@gmail.com>
2018-07-18 22:35:24 -0700

    Implement moment-timezone to format date and time for Denver

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import faker from 'faker';
+import moment from 'moment-timezone'; // both moment and moment-timezone are installed
 
 Vue.use(Vuex);
 
@@ -25,10 +26,12 @@ export default new Vuex.Store({
       const mockData = new Array(5).fill(null)
         .map(() => {
           // get a complete date, break this up into day of week and time later
-          const date = faker.date.between('2018-08-01', '2018-12-01');
+          const date = moment(faker.date.between('2018-08-01', '2018-12-01'))
+            .tz('America/Denver'); // format as moment
           return {
             title: faker.lorem.words(3),
-            calendarDate: date,
+            calendarDate: date.format('dddd, MMMM DD, YYYY'), // example - Tuesday, October 02, 2018
+            time: date.format('h:mm z'), // example 1:35 MDT
             imageURL: faker.image.dataUri(800, 600), // blank square since lorem pixel is quite slow
             // imageURL: faker.image.imageUrl(800, 600, 'abstract'),
             additionalInfo: faker.lorem.words(4),
