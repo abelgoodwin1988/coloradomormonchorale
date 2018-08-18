38325113ca814571bde90eff72f2b2ceaa785364
Jacob Goodwin <jacob.goodwin@gmail.com>
2018-08-18 14:40:12 -0500

    Setup basic axios config in main.js

--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+import axios from 'axios';
 
 Vue.config.productionTip = false;
+
+
+axios.defaults = {
+  baseURL: 'http://localhost:3000', // for development dB
+};
 
 // Require the main Sass manifest file
 require('./assets/sass/main.scss');
