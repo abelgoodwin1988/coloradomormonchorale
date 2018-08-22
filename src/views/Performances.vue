9daf0e9a8b6b432c8f2d3e95d8188cb3c7e19297
Jacob Goodwin <jacob.goodwin@gmail.com>
2018-08-22 12:13:38 -0500

    Remove duplicate hero and hero-body divs

--- src/views/Performances.vue
+++ src/views/Performances.vue
@@ -1,38 +1,34 @@
 <template>
     <div class="auditions">
         <section class="hero is-link is-bold is-fullheight">
-            <section class="hero">
-                <div class="hero-body">
-                    <div class="container">
-                        <section class="hero">
-                            <div class="hero-body">
-                                <div class="container">
-                                    <h1 class="title">
-                                        Performances
-                                    </h1>
-                                    <div
-                                      class="box"
-                                      v-for="(event,index) in events"
-                                      :key="index">
-                                      <article class="media">
-                                        <div class="media-left">
-                                          <figure class="image is-4by3">
-                                            <img :src="event.imageURL">
-                                          </figure>
-                                        </div>
-                                        <div class="media-content">
-                                          <div class="content">
-                                            <h3>{{ event.title }}</h3>
-                                          </div>
-                                        </div>
-                                      </article>
-                                    </div>
+          <div class="container">
+              <section class="hero">
+                  <div class="hero-body">
+                      <div class="container">
+                          <h1 class="title">
+                              Performances
+                          </h1>
+                          <div
+                            class="box"
+                            v-for="(event,index) in events"
+                            :key="index">
+                            <article class="media">
+                              <div class="media-left">
+                                <figure class="image is-4by3">
+                                  <img :src="event.imageURL">
+                                </figure>
+                              </div>
+                              <div class="media-content">
+                                <div class="content">
+                                  <h3>{{ event.title }}</h3>
                                 </div>
-                            </div>
-                        </section>
-                    </div>
-                </div>
-            </section>
+                              </div>
+                            </article>
+                          </div>
+                      </div>
+                  </div>
+              </section>
+          </div>
         </section>
     </div>
 </template>
