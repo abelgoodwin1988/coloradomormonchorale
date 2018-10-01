96bb28f266626807f199d154630aa29852ee2936
abelgoodwin1988 <abelgoodwin1988@gmail.com>
2018-09-30 22:12:40 -0600

    fix spacing to 4

--- src/views/Performances.vue
+++ src/views/Performances.vue
@@ -2,42 +2,42 @@
 <!-- eslint-disable -->
     <div class="performances">
         <section class="hero is-link is-bold is-fullheight">
-          <section class="hero">
-            <div class="hero-body">
-                <div class="container">
-                  <section class="hero">
-                      <div class="hero-body">
-                          <div class="container">
-                              <h1 class="title">
-                                  Performances
-                              </h1>
-                              <div
-                                class="box content"
-                                v-for="(event) in events"
-                                :key="event.id">
-                                <div class="columns is-centered">
-                                  <div class="column is-one-third-desktop is-one-third-tablet">
-                                    <img :src="event.imageURL" alt="Event Image">
-                                  </div>
-                                  <div class="column">
-                                    <h2 class="has-text-link">{{ event.title }}</h2>
-                                    <h5 class="has-text-grey">{{ event.time }}</h5>
-                                    <h5 class="has-text-grey-darker">{{ event.address.location }}</h5>
-                                    <p class="has-text-grey-dark is-size-6">
-                                      {{ event.address.street1 }} <br />
-                                      <span v-if="event.address.street2">{{ event.address.street2 }} <br /></span>
-                                      {{ event.address.city }}, {{event.address.state}} {{event.address.zip}}
-                                    </p>
-                                    <h6 class="has-text-info">{{ event.otherInfo }}</h6>
-                                  </div>
+            <section class="hero">
+                <div class="hero-body">
+                    <div class="container">
+                    <section class="hero">
+                        <div class="hero-body">
+                            <div class="container">
+                                <h1 class="title">
+                                    Performances
+                                </h1>
+                                <div
+                                    class="box content"
+                                    v-for="(event) in events"
+                                    :key="event.id">
+                                    <div class="columns is-centered">
+                                    <div class="column is-one-third-desktop is-one-third-tablet">
+                                        <img :src="event.imageURL" alt="Event Image">
+                                    </div>
+                                    <div class="column">
+                                        <h2 class="has-text-link">{{ event.title }}</h2>
+                                        <h5 class="has-text-grey">{{ event.time }}</h5>
+                                        <h5 class="has-text-grey-darker">{{ event.address.location }}</h5>
+                                        <p class="has-text-grey-dark is-size-6">
+                                        {{ event.address.street1 }} <br />
+                                        <span v-if="event.address.street2">{{ event.address.street2 }} <br /></span>
+                                        {{ event.address.city }}, {{event.address.state}} {{event.address.zip}}
+                                        </p>
+                                        <h6 class="has-text-info">{{ event.otherInfo }}</h6>
+                                    </div>
+                                    </div>
                                 </div>
-                              </div>
-                          </div>
-                      </div>
-                  </section>
+                            </div>
+                        </div>
+                    </section>
+                    </div>
                 </div>
-              </div>
-          </section>
+            </section>
         </section>
     </div>
 </template>
