d49d3620205134248516871fd58c3fb0344dfb19
Jacob Goodwin <jacob.goodwin@gmail.com>
2018-11-03 23:39:32 -0700

    Verify that we have read access to performances, but not write

--- src/views/admin/Login.vue
+++ src/views/admin/Login.vue
@@ -7,11 +7,33 @@
 </template>
 
 <script>
+import { performancesRef } from '@/firebase/firebaseInit';
+
 export default {
   data() {
     return {
       title: 'The Admin Login Page!',
     };
+  },
+  created() {
+    // test if we can get performances!
+    performancesRef.get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(doc.id, ' => ', doc.data());
+      });
+    }).catch((error) => {
+      console.log('Error getting documents: ', error);
+    });
+
+    // However, we should not be able to write without auth
+    performancesRef.add({
+      title: 'A performance of epic proportions!',
+    }).then((docRef) => {
+      console.log('Document written with ID: ', docRef.id);
+    }).catch((error) => {
+      console.error('Error adding document: ', error);
+    });
   },
 };
 </script>
