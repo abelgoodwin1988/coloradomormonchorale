83853411a132f2514257786043d984873c95e16e
abelgoodwin1988 <abelgoodwin1988@gmail.com>
2019-02-23 13:24:04 -0700

    fix eol lint issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,21 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  'extends': [
+  extends: [
     'plugin:vue/essential',
-    '@vue/airbnb'
+    '@vue/airbnb',
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-param-reassign': ['error', { 
-      'props': true, 
-      "ignorePropertyModificationsFor": ['state'] }], //for vuex state mutations
+    'no-param-reassign': ['error', {
+      props: true,
+      ignorePropertyModificationsFor: ['state'],
+    }], // for vuex state mutations
   },
   parserOptions: {
-    parser: 'babel-eslint'
-  }
-}
+    parser: 'babel-eslint',
+  },
+};
